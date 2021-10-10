4a3b01b45f0397e565912fdff880a8d559712468
Leonardo Eichstaedt <leonardoeichs@gmail.com>
2021-10-09 23:09:23 -0300

    finish weather card

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -2,19 +2,58 @@ import WeatherCard from "../WeatherCard";
 
 import { Container, SearchBar, SearchButton, Input } from "./styles";
 
+import { useState } from "react";
+
+const URL =
+  "http://api.weatherapi.com/v1/forecast.json?key=b5e57d17623741609f8231655210910&days=5&aqi=no&alerts=no";
+
 function Search() {
+  const [searchValue, setSearchValue] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState(null);
+  const [cardVisible, setCardVisible] = useState(true);
+
+  const searchButton = (e, searchTerm) => {
+    e.preventDefault();
+    if (!!searchTerm)
+      fetch(`${URL}&q=${searchTerm}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setWeatherInfo(data);
+          setCardVisible(true);
+        })
+        .catch((err) => {
+          setWeatherInfo(null);
+          setCardVisible(false);
+        });
+    else {
+      setWeatherInfo(null);
+      setCardVisible(false);
+    }
+  };
+
   return (
     <Container>
       <h1>Previsão do tempo</h1>
-      <WeatherCard />
+      {weatherInfo != null && (
+        <WeatherCard
+          weatherInfo={weatherInfo}
+          cardVisible={cardVisible}
+          setCardVisible={setCardVisible}
+        />
+      )}
       <SearchBar>
         <Input
           placeholder="Insira aqui o nome da cidade"
+          value={searchValue}
           type="search"
           maxLength="60"
+          onChange={(e) => setSearchValue(e.target.value)}
         />
-        <SearchButton type="submit">
-          <i class="fa fa-search"></i>
+        <SearchButton
+          type="submit"
+          onClick={(e) => searchButton(e, searchValue)}
+        >
+          <i className="fa fa-search"></i>
         </SearchButton>
       </SearchBar>
     </Container>
--- src/components/WeatherCard/index.js
+++ src/components/WeatherCard/index.js
@@ -10,64 +10,76 @@ import {
   WeatherMain,
 } from "./styles";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function WeatherCard() {
-  const [cardVisible, setCardVisible] = useState(true);
+const weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
 
+function WeatherCard({ weatherInfo, cardVisible, setCardVisible }) {
   return (
     <Container style={{ display: cardVisible ? "block" : "none" }}>
       <CardHeader>
-        <h1>Niterói, RJ - Brasil</h1>
+        <h1>
+          {weatherInfo.location.name} - {weatherInfo.location.region}
+        </h1>
         <CloseButton onClick={() => setCardVisible(!cardVisible)}>
           <i class="fa fa-times"></i>
         </CloseButton>
       </CardHeader>
       <WeatherMain>
         <h2 className="temperature">
-          <span>20˚C</span> Nublado
+          <span>{Math.round(weatherInfo.current.temp_c)}˚C</span>{" "}
+          {weatherInfo.current.condition.text}
         </h2>
       </WeatherMain>
       <WeatherInfo>
         <MinMaxTemp>
           <div className="min-max">
             <span className="icon arrows">&#8595;</span>
-            <span>16˚</span>
+            <span>
+              {Math.round(weatherInfo.forecast.forecastday[0].day.mintemp_c)}˚
+            </span>
           </div>
           <div className="min-max">
             <span className="icon arrows">&#8593;</span>
-            <span>25˚</span>
+            <span>
+              {Math.round(weatherInfo.forecast.forecastday[0].day.maxtemp_c)}˚
+            </span>
           </div>
         </MinMaxTemp>
         <p>
-          Sensação <span className="bold">19˚</span>
+          Sensação{" "}
+          <span className="bold">
+            {Math.round(weatherInfo.current.feelslike_c)}˚
+          </span>
         </p>
         <p>
-          Vento <span className="bold">18km/h</span>
+          Vento <span className="bold">{weatherInfo.current.wind_kph}km/h</span>
         </p>
         <p>
-          Humidade <span className="bold">89%</span>
+          Humidade <span className="bold">{weatherInfo.current.humidity}%</span>
         </p>
       </WeatherInfo>
 
       <HorizontalLine />
       <WeekTemperature>
-        <Day>
-          <p className="day-name">Terça</p>
-          <p className="day-temperature">18˚ 26˚</p>
-        </Day>
-        <Day>
-          <p className="day-name">Terça</p>
-          <p className="day-temperature">18˚ 26˚</p>
-        </Day>
-        <Day>
-          <p className="day-name">Terça</p>
-          <p className="day-temperature">18˚ 26˚</p>
-        </Day>
-        <Day>
-          <p className="day-name">Terça</p>
-          <p className="day-temperature">18˚ 26˚</p>
-        </Day>
+        {weatherInfo.forecast.forecastday.map((dayOfWeek, ind) => (
+          <Day key={ind}>
+            <p className="day-name">
+              {weekday[new Date(dayOfWeek.date).getUTCDay()]}
+            </p>
+            <p className="day-temperature">
+              {Math.round(dayOfWeek.day.mintemp_c)}˚{" "}
+              {Math.round(dayOfWeek.day.maxtemp_c)}˚
+            </p>
+          </Day>
+        ))}
       </WeekTemperature>
     </Container>
   );
