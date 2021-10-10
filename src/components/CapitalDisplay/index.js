584c8a8e8e929ba26fb1ee0399597f4ffd2c849c
Leonardo Eichstaedt <leonardoeichs@gmail.com>
2021-10-09 21:12:18 -0300

    add dynamic data do capital display

--- src/components/CapitalDisplay/index.js
+++ src/components/CapitalDisplay/index.js
@@ -1,6 +1,40 @@
 import { Container, Row, MinMaxHeader } from "./styles";
+import { useEffect, useState } from "react";
+
+const capitals = [
+  "Rio de Janeiro",
+  "São Paulo",
+  "Belo Horizonte",
+  "Brasília",
+  "Belém",
+  "Salvador",
+  "Curitiba",
+  "Fortaleza",
+  "Manaus",
+  "Joao Pessoa",
+];
+
+const URL =
+  "http://api.weatherapi.com/v1/forecast.json?key=b5e57d17623741609f8231655210910&days=1&aqi=no&alerts=no";
 
 function CapitalDisplay() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    capitals.map((capital) => {
+      fetch(`${URL}&q=${capital}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setWeatherInfo((prevState) => [...prevState, data]);
+        })
+        .catch((error) => setError(error));
+    });
+    setIsLoading(false);
+  }, []);
+
   return (
     <Container>
       <h1>Capitais</h1>
@@ -22,44 +56,25 @@ function CapitalDisplay() {
           </li>
         </ul>
       </MinMaxHeader>
-
-      <ul>
-        <li>
-          <Row>
-            <div>18˚</div>
-            <div>27˚</div>
-            <div>Rio de Janeiro</div>
-          </Row>
-        </li>
-        <li>
-          <Row>
-            <div>18˚</div>
-            <div>27˚</div>
-            <div>Rio de Janeiro</div>
-          </Row>
-        </li>
-        <li>
-          <Row>
-            <div>18˚</div>
-            <div>27˚</div>
-            <div>Rio de Janeiro</div>
-          </Row>
-        </li>
-        <li>
-          <Row>
-            <div>18˚</div>
-            <div>27˚</div>
-            <div>Rio de Janeiro</div>
-          </Row>
-        </li>
-        <li>
-          <Row>
-            <div>18˚</div>
-            <div>27˚</div>
-            <div>Rio de Janeiro</div>
-          </Row>
-        </li>
-      </ul>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {weatherInfo.map((weather, i) => (
+            <li key={i}>
+              <Row>
+                <div>
+                  {Math.round(weather.forecast.forecastday[0].day.mintemp_c)}˚
+                </div>
+                <div>
+                  {Math.round(weather.forecast.forecastday[0].day.maxtemp_c)}˚
+                </div>
+                <div>{weather.location.name}</div>
+              </Row>
+            </li>
+          ))}
+        </ul>
+      )}
     </Container>
   );
 }
